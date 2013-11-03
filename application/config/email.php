b3120e444406ea446193f7544ba17b7b1838dffc
rkchaux@gmail.com <rkchaux@gmail.com>
2013-11-03 23:23:46 +0530

    added documentation to setup

--- application/config/email.php
+++ application/config/email.php
@@ -1,13 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+	You can enable/disable email sending with following flag
+*/
 $config['enable'] = FALSE;
 
+/*
+	Configure SMTP information for sending email
+*/
 $config['smtp'] = array();
 $config['smtp']['user'] = "";
 $config['smtp']['password'] = "";
 $config['smtp']['host'] = "ssl://smtp.googlemail.com";
 $config['smtp']['port'] = 465;
 
+/*
+	Configure email of the sender. Used as the `from` email when sending emails
+*/	
 $config['sender'] = array();
 $config['sender']['email'] = "admin@saas.com";
 $config['sender']['name'] = "SAAS Admin";
---
+++ readme.md
@@ -0,0 +1,17 @@
+SAAS Boilerplate
+================
+
+Requirements
+------------
+
+* PHP 5.x
+* MySql
+* Apache Server
+
+Installation
+------------
+
+* Add this project into your web root
+* Use `database.sql` to create the initial database
+* configure database settings at `application/config/database.php`
+* configure email settings at `application/config/email.php`
