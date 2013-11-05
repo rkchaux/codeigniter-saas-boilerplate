f28e590871e436613ed34af64638160a3e14853e
rkchaux@gmail.com <rkchaux@gmail.com>
2013-11-05 08:00:28 +0530

    removed updateMetricForUser helper API. which seems useless

--- application/helpers/plan_helper.php
+++ application/helpers/plan_helper.php
@@ -9,17 +9,3 @@ function updateMetric($companyId, $metricName, $qty) {
 	$CI->load->model("plan_model");
 	$CI->plan_model->updateMetric($companyId, $metricName, $qty);
 }
-
-/*
-	Update the given metric for the currently selected company of loggedIn user
-*/
-function updateMetricForUser($metricName, $qty) {
-
-	$CI = &get_instance();
-
-	$email = $CI->session->userdata("email");
-	$companyInfo = $CI->session->userdata("company");
-
-	$CI->load->model("plan_model");
-	$CI->plan_model->updateMetric($companyInfo['id'], $metricName, $qty);
-}
